21d8e2e3d17e6721fe180a6f4d8eebf574d9f604
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-04-25 23:31:00 +0530

    Deleted previous version

--- src/components/UI/Backdrop/Backdrop.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import './Backdrop.css'
-
-export default function Backdrop(props) {
-  return props.show ? <div className="Backdrop" onClick={props.click}></div> : null;
-}
