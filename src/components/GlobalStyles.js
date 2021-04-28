9ca15b54848d151dd7dcc0d3cbcbcf1977a46743
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-04-29 04:38:13 +0530

    created a global style component

---
+++ src/components/GlobalStyles.js
@@ -0,0 +1,57 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+*{
+    margin:0;
+    padding: 0%;
+    box-sizing: border-box;
+}
+
+html{
+    ::-webkit-scrollbar {
+        width: 5px;
+      }
+      
+      /* Track */
+      ::-webkit-scrollbar-track {
+        background: #888; 
+      }
+       
+      /* Handle */
+      ::-webkit-scrollbar-thumb {
+        background: #666; 
+      }
+      
+      /* Handle on hover */
+      ::-webkit-scrollbar-thumb:hover {
+        background: #333; 
+      }
+}
+body{
+    font-family: 'Nunito', sans-serif;
+    width:100%;
+    background-color: #1a1a1a;
+}
+h2{
+    font-size: 2.5rem;
+    font-weight: 600;
+    color: #fff;
+}
+h3{
+    font-size: 1.25rem;
+    color: #eee;
+
+}
+p{
+    font-size:1.2rem;
+    line-height:200%;
+    color: #aaa;
+}
+a{
+    text-decoration:none;
+    color: #999;
+}
+
+`;
+
+export default GlobalStyles;
