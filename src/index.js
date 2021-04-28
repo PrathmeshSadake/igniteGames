19f3b72146462f92f2203277e8c6cfac37699988
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-04-28 12:16:38 +0530

    setting thunk

--- src/index.js
+++ src/index.js
@@ -3,4 +3,31 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// REDUX SETUP
+/* Creates a store enhancer that applies middleware
+to the dispatch method of the Redux store.
+This is handy for a variety of tasks,
+such as expressing asynchronous actions in a concise manner,
+or logging every action payload.
+*/ import {
+  createStore,
+  applyMiddleware,
+  compose,
+} from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+// ROOT REDUCER
+import rootReducer from "./reducers";
+
+// Combines redux dev tools and thunk in one line for us.
+//Check Redux dev tools docs for more info.
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
